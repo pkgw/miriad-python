876a12ffda45a14ec93fa7c1f187efd12d3d5905
Peter Williams <peter@newton.cx>
2008-07-17 16:15:57 -0700

    Add mirpymodtask to help tasks implemented as modules.

---
+++ mirpymodtask.py
@@ -0,0 +1,24 @@
+#! /bin/sh
+#
+# This is a wrapper script for Miriad-Python tasks that are
+# implemented as Python modules. (This is something you might
+# want to do if you want the task to be directly callable from
+# other Python code.)
+#
+# This wrapper should be used by creating a symlink to this file. The
+# symlink should have the same name as the module (minus the .py
+# extension) and be located in a directory in $PATH.
+#
+# This script basically runs "python -m modname args...". This syntax
+# looks up the module "modname" in Python's module search path and
+# runs it as a script. The module should have some code that looks like
+#
+# if __name__ == '__main__':
+#    run as task
+#
+# that implements the module as a task. You need to protect the task
+# code in this way because otherwise, it will be run unconditionally
+# when the module is imported by other Python code, which is not
+# desireable, or polite, behavior.
+
+exec python -m `basename $0` "$@"
