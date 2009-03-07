dbcec82fab5e2e09ecbb3fbc642daa3765339537
Peter Williams <peter@newton.cx>
2009-03-07 15:33:49 -0800

    OK, I think miriad.py is fully documented.

--- miriad.py
+++ miriad.py
@@ -9,6 +9,19 @@ from stat import ST_MTIME
 launchTrace = None
 
 def basicTrace ():
+    """Set the tracing function :data:`launchTrace` to a basic default.
+
+:returns: :const:`None`
+
+Sets the tracing function :data:`launchTrace` to a function that
+prints out every command invoked, prefixed by "MIRIAD: ". Command
+parameters are surrounded by single quotes to facilitate copying and
+pasting into a shell.
+
+The tracing function is stored in the variable :data:`launchTrace`. An
+action can be traced by calling :func:`trace`.
+"""
+
     global launchTrace
 
     def t (cmd):
@@ -18,6 +31,20 @@ def basicTrace ():
     launchTrace = t
 
 def trace (cmd):
+    """Trace the execution of *cmd*.
+
+:arg cmd: a command and arguments
+:type cmd: list of string
+:returns: :const:`None`
+
+Invokes the callable :data:`launchTrace` with *cmd* as the argument,
+unless :data:`launchTrace` is :const:`None`, in which case this
+function is a noop.
+
+The function :func:`basicTrace` sets :data:`launchTrace` to a simple
+default.
+"""
+
     if launchTrace is not None:
         launchTrace (cmd)
 
@@ -27,12 +54,14 @@ __all__ = ['basicTrace', 'trace']
 # Fundamental MIRIAD data set object.
 
 class Data (object):
-    """:synopsis: Generic reference to MIRIAD datasets.
-:arg: basedata
-:type: anything upon which :func:`str` can be called
+    """:synopsis: Generic reference to a MIRIAD dataset.
+:arg basedata: The filename of the underyling dataset.
+:type basedata: anything upon which str() can be called
 
 The constructor returns a new instance that references a dataset with
-the filename *basedata*.
+the filename ``str(basedata)``. However, you should create
+:class:`VisData` and :class:`ImData` instances rather than generic
+:class:`Data` instances whenever possible.
 
 The stringification of a :class:`Data` instance returns the filename
 of the underlying dataset.
@@ -60,7 +89,6 @@ filenames underlying both datasets are equal.
         return isinstance (other, Data) and self.realPath () == other.realPath ()
 
     def __hash__ (self):
-        "Test documentation."""
         return hash (self.realPath ())
     
     # Low-level attributes
@@ -85,6 +113,7 @@ attribute::
     # If dest doesn't exist or src has been modified more
     # recently than src, perform a uvcat.
     if dest.umtime < src.mtime:
+       dest.delete ()
        src.catTo (dest, **params)
 """
         # Once the set is created, it can be modified without updating
@@ -116,8 +145,8 @@ and return. If not, raise an :class:`Exception`."""
 
     def realPath (self):
         """:rtype: :class:`str`
-:returns: The real path (as defined by :func:`os.path.realpath`
-          to this dataset.
+:returns: The real path (as defined by :func:`os.path.realpath`)
+  to this dataset.
 """
         return os.path.realpath (self.base)
     
@@ -126,8 +155,8 @@ and return. If not, raise an :class:`Exception`."""
     def moveTo (self, dest):
         """Move the dataset to a new location.
 
-:arg: dest
-:type: :class:`str`, or anything upon which :func:`str` can be called.
+:arg dest: the new filename for the dataset 
+:type dest: :class:`str`, or anything upon which :func:`str` can be called.
 :rtype: :class:`~Data`
 :returns: :const:`self`
 
@@ -151,14 +180,14 @@ called with a "[rename]" operation.
     def copyTo (self, dest):
         """Copy the dataset to a new location.
 
-:arg: dest
-:type: :class:`str`, or anything upon which :func:`str` can be called.
+:arg dest: the filename of the copy of the dataset
+:type dest: :class:`str`, or anything upon which :func:`str` can be called.
 :rtype: :class:`~Data`
 :returns: :const:`self`
 
 Copies this dataset to a new location on disk. The dataset must
-exist. Approximately equivalent to :command:`cp -r $self
-$dest`. :func:`trace` is called with a "[copy]" operation.
+exist. Approximately equivalent to :command:`cp -r {self}
+{dest}`. :func:`trace` is called with a "[copy]" operation.
 
 FIXME: Doesn't clean up correctly if an error occurs midway
 through the copy.
@@ -182,11 +211,11 @@ through the copy.
         """Delete the dataset.
 
 :rtype: :class:`~Data`
-:returns: :const:self
+:returns: :const:`self`
 
 Deletes the dataset from disk. If the dataset doesn't exist,
 silently does nothing. Approximately equivalent to
-:command:`rm -r $self`. If deletion occurs, :func:`trace` is called
+:command:`rm -r {self}`. If deletion occurs, :func:`trace` is called
 with a "[delete]" operation.
 """
         if not self.exists: return
@@ -202,10 +231,9 @@ with a "[delete]" operation.
     def apply (self, task, **params):
         """Configure a task to run on this dataset.
 
-:arg: task
-:type: :class:`~mirexec.TaskBase`
-:arg: params
-:type: Extra keyword parameters
+:arg task: the task to set up
+:type task: :class:`~mirexec.TaskBase`
+:arg params: extra task parameters
 :rtype: :class:`~mirexec.TaskBase`
 :returns: *task*
 
@@ -233,10 +261,9 @@ to set the appropriate task input keyword.
     def xapply (self, task, **params):
         """Configure a task to run verbosely on this dataset.
 
-:arg: task
-:type: :class:`~mirexec.TaskBase`
-:arg: params
-:type: Extra keyword parameters
+:arg task: the task to set up
+:type task: :class:`~mirexec.TaskBase`
+:arg params: extra task parameters
 :rtype: :class:`~mirexec.TaskBase`
 :returns: *task*
 
@@ -251,6 +278,32 @@ Identical to :meth:`apply`, but also sets the
     # Programming-related helpers.
     
     def makeVariant (self, name, kind=None):
+        """Create another :class:`Data` instance with a similar name.
+
+:arg name: the extension to to append to the original dataset's name
+:type name: :class:`str`
+:arg kind: the factory method to create the new dataset
+:type kind: callable, returning :class:`Data`; if :const:`None`, use :class:`Data`
+:rtype: :class:`Data`
+:returns: a new dataset.
+
+Creates a new dataset with an underlying filename of
+:file:`{self}.{name}`. For example::
+
+  v = VisData ('orig')
+  d = v.makeVariant ('avg')
+  v.averTo (d, 10)
+  # Executes: uvaver vis=orig out=orig.avg interval=10
+
+The argument *kind* specifies the factory function used to create
+the instance. A class name or a custom function taking one string
+argument are appropriate.
+
+For the common case, the more tersely-named functions :meth:`vvis` and
+:meth:`vim` will create new :class:`VisData` and :class:`ImData`
+variants, respectively.
+"""
+
         if kind is None: kind = Data # for some reason kind=Data barfs.
 
         inst = kind (self.base + '.' + name)
@@ -262,6 +315,32 @@ Identical to :meth:`apply`, but also sets the
 
     @classmethod
     def defaultVisClass (klass, vclass):
+        """Set the class used by :meth:`vvis`.
+
+:arg vclass: the class to be used by :func:`vvis`
+:type vclass: any subclass of :class:`VisData`
+:raises ValueError: if *vclass* is not a subclass 
+  of :class:`VisData`
+
+Sets the class instantiated by *all* calls to :meth:`vvis`. This
+method is a class method, and so sets the value used by all
+instances of :class:`Data`. This can be useful if you have a
+customized subclass of :class:`VisData` that you want to use
+throughout a program::
+
+  class MyVis (VisData):
+    def myfunc (self):
+      # Perform some useful function ...
+
+  Data.defaultVisClass (MyVis)
+
+  v = MyVis ('foo')
+  v2 = v.vvis ('avg')
+  # v2 is an instance of MyVis, too
+
+*vclass* must be a subclass of :class:`VisData`.
+"""
+
         if not issubclass (vclass, VisData):
             raise ValueError ('vclass')
 
@@ -269,15 +348,65 @@ Identical to :meth:`apply`, but also sets the
 
     @classmethod
     def defaultImClass (klass, iclass):
+        """Set the class used by :meth:`vim`.
+
+:arg: iclass: the class to be used by :func:`vim`
+:type iclass: any subclass of :class:`ImData`
+:raises ValueError: if *iclass* is not a subclass 
+  of :class:`ImData`
+
+Sets the class instantiated by *all* calls to :meth:`vim`. This
+method is a class method, and so sets the value used by all
+instances of :class:`Data`. This can be useful if you have a
+customized subclass of :class:`ImData` that you want to use
+throughout a program::
+
+  class MyIm (ImData):
+    def myfunc (self):
+      # Perform some useful function ...
+
+  Data.defaultImClass (MyIm)
+
+  im = MyIm ('foo')
+  im2 = im.vim ('avg')
+  # im2 is an instance of MyIm, too
+
+*iclass* must be a subclass of :class:`ImData`.
+"""
+
         if not issubclass (iclass, ImData):
             raise ValueError ('iclass')
 
         klass._defImClass = iclass
 
     def vvis (self, name):
+        """Create a :class:`VisData` variant of this dataset.
+
+:arg name: the extension to append to the dataset's name
+:type name: :class:`str`
+:rtype: :class:`VisData`
+:returns: a new dataset object
+
+Returns ``self.makeVariant (name, klass)`` where *klass* is, by
+default, :class:`VisData`. The value of *klass* can be overridden by
+calling :meth:`defaultVisClass`.
+"""
+
         return self.makeVariant (name, self._defVisClass)
     
     def vim (self, name):
+        """Create an :class:`ImData` variant of this dataset.
+
+:arg name: the extension to append to the dataset's name
+:type name: :class:`str`
+:rtype: :class:`ImData`
+:returns: a new dataset object
+
+Returns ``self.makeVariant (name, klass)`` where *klass* is, by
+default, :class:`ImData`. The value of *klass* can be overridden by
+calling :meth:`defaultImClass`.
+"""
+
         return self.makeVariant (name, self._defImClass)
 
     # Detailed access to dataset.
@@ -295,6 +424,22 @@ Identical to :meth:`apply`, but also sets the
         return UserDataSet (self, create)
     
     def open (self, mode):
+        """Opens the dataset for access.
+
+:arg mode: The mode to open the dataset in. Use 'r' to open the
+  dataset in read-only mode and 'w' to open it in read-write mode,
+  possibly creating the dataset in the process.
+:type mode: :class:`str`
+:rtype: :class:`mirtask.UserDataSet`
+:returns: An opened dataset
+
+This implementation opens the dataset in a generic way. To be able to
+access visibility or image data through the usual functions, make sure
+to call this function on an instance of the :class:`VisData` or
+:class:`ImData` classes, which override the  implementation to open
+the dataset in the correct manner for their corresponding data types.
+"""
+
         return self._openImpl (mode)
 
 __all__ += ['Data']
@@ -303,11 +448,34 @@ __all__ += ['Data']
 # Visibility data
 
 class VisData (Data):
+    """:synopsis: Reference to a MIRIAD visibility dataset.
+
+This subclass of :class:`Data` is for referring to visibility
+datasets. It has special functions handy for use with visibility data
+and inherits many generic features from the :class:`Data` class.
+"""
+
     def _openImpl (self, mode):
         from mirtask import UVDataSet
         return UVDataSet (self, mode)
 
     def readLowlevel (self, saveFlags, **kwargs):
+        """Directly access the visibility data.
+
+:arg saveFlags: whether to save modified UV flags to the dataset
+  as it is read
+:type saveFlags: :class:`bool`
+:arg kwargs: extra arguments to
+  :func:`mirtask.uvdat.readFileLowlevel`.
+:rtype: generator
+:returns: generates a sequence of UV information tuples. See the
+  documentation of :func:`mirtask.uvdat.readFileLowlevel` for more
+  information.
+
+Calls :func:`mirtask.uvdat.readFileLowlevel` on this dataset with the
+specified arguments.
+"""
+
         from mirtask import uvdat
         return uvdat.readFileLowlevel (self.base, saveFlags, **kwargs)
     
@@ -319,12 +487,43 @@ class VisData (Data):
         return task
 
     def catTo (self, dest, **params):
+        """:command:`uvcat` this dataset to *dest*.
+
+:arg dest: the destination dataset
+:type dest: :class:`Data`, str, or any other stringable
+:arg params: extra parameters to pass to the
+  :command:`uvcat` command
+:rtype: :const:`None`
+
+Invokes the task :command:`uvcat` via
+:class:`mirexec.TaskUVCat`. Checks if the source dataset exists
+but does no checking on the destination dataset. Approximately
+equivalent to executing :command:`uvcat vis={self} out={dest}
+{params...}`.
+"""
+
         self.checkExists ()
 
         from mirexec import TaskUVCat
         self.apply (TaskUVCat, out=dest, **params).run ()
 
     def averTo (self, dest, interval, **params):
+        """:command:`uvaver` this dataset to *dest*.
+
+:arg dest: the destination dataset
+:type dest: :class:`Data`, str, or any other stringable
+:arg interval: the averaging interval, in minutes
+:type interval: numeric
+:arg params: extra parameters to pass to the
+  :command:`uvaver` command
+:rtype: :const:`None`
+
+Invokes the task :command:`uvaver` via
+:class:`mirexec.TaskUVAver`. Checks if the source dataset exists
+but does no checking on the destination dataset. Approximately
+equivalent to executing :command:`uvaver vis={self} out={dest}
+interval={interval} {params...}`.
+"""
         self.checkExists ()
 
         from mirexec import TaskUVAver
@@ -338,6 +537,12 @@ __all__ += ['VisData']
 # Image data
 
 class ImData (Data):
+    """:synopsis: Reference to a MIRIAD image dataset.
+
+This subclass of :class:`Data` is for referring to image datasets. It
+inherits many generic features from the :class:`Data` class.
+"""
+
     # FIXME: haven't yet wrapped XYIO routines to
     # make it possible to open and read images.
 
