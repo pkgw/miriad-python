03cd75d810f549d971d3c647ae0d2929c8b4bde7
Peter Williams <peter@newton.cx>
2011-04-28 10:34:08 -0700

    mirtask: add copyright headers to _uvdat_compat files

--- mirtask/_uvdat_compat_24.py
+++ mirtask/_uvdat_compat_24.py
@@ -1,6 +1,23 @@
 """Python-2.4-compatible implementations of a few functions in mirtask.uvdat.
 See _uvdat_compat_default.py for the reference implementations."""
 
+# Copyright 2009, 2010, 2011 Peter Williams
+#
+# This file is part of miriad-python.
+#
+# Miriad-python is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Miriad-python is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with miriad-python.  If not, see <http://www.gnu.org/licenses/>.
+
 import lowlevel as ll
 
 def _inputSets (UVDatDataSet):
--- mirtask/_uvdat_compat_default.py
+++ mirtask/_uvdat_compat_default.py
@@ -3,6 +3,23 @@ with Python 2.4 that raise SyntaxErrors. You can't catch them in
 the module itself, but you can catch them on import, so this allows
 us to use the ideal functions if possible but fall back if necessary."""
 
+# Copyright 2009, 2010, 2011 Peter Williams
+#
+# This file is part of miriad-python.
+#
+# Miriad-python is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Miriad-python is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with miriad-python.  If not, see <http://www.gnu.org/licenses/>.
+
 import lowlevel as ll
 
 def _inputSets (UVDatDataSet):
